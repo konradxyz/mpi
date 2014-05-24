36523c7a1ac7c712d37df1de9a227504dcc83c1c
Konrad Paziewski <kp306410@students.mimuw.edu.pl>
2014-05-24 02:14:03 +0200

    pack

--- pack.sh
+++ pack.sh
@@ -2,5 +2,5 @@
 
 make clean
 rm msp_kp306410.tar.gz
-tar -cvvf msp_kp306410.tar.gz Makefile matgen.h matgen-mt.c msp-par.c msp-seq-naive.c msp-par.ll
+tar -cvvf msp_kp306410.tar.gz Makefile matgen.h matgen-mt.c msp-par.c msp-seq-naive.c msp-par.ll gen_tasks.sh msp-par-template.ll
 
